b2bd8a93a40e67663d9d551c03899e02e3967c78
zladdi <zladdi@users.noreply.github.com>
2016-12-16 16:38:46 +0100

    Initial commit

---
+++ merge-doc-osx/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  merge-doc-osx
+//
+//  Created by zlatko on 16/12/16.
+//  Copyright © 2016 Zlatko Franjcic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
